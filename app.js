afb908c9abe5c0617757304aad57834572023b75
EARNEST <earnytechlive@gmail.com>
2022-12-22 21:36:53 +0300

    creating div and li element in js

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ todoButton.addEventListener('click', addTodo)
 
 // functions
 function addTodo(event){
+
+    //create div
     event.preventDefault();
-    
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
 }
