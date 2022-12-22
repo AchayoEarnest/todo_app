b4d9c941c15af187a4ed2410036a3ff0428e360a
EARNEST <earnytechlive@gmail.com>
2022-12-22 22:24:38 +0300

    creating div and li element in js

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 // selectors
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 // event listeners
-todoButton.addEventListener('click', addTodo)
-
-
+todoButton.addEventListener("click", addTodo);
 
 // functions
-function addTodo(event){
+function addTodo(event) {
+  //create div
+  event.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
 
-    //create div
-    event.preventDefault();
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
+  //create li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
 
-    //create li
-    const newTodo = document.createElement('li');
-    newTodo.innerText = 'hey';
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
+  //check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerText = "Done";
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  //trash
+  const trashButton = document.createElement("button");
+  trashButton.innerText = "Delete";
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  //append to list
+  todoList.appendChild(todoDiv);
 }
